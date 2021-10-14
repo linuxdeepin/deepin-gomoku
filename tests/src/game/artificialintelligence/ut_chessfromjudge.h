dcdec2ed061e67c2e43d7a6c0e15c437462b617e
liyucheng <liyucheng@uniontech.com>
2021-10-14 14:40:23 +0800

    feat: 增加AI单元测试
    增加AI单元测试
    
    Log: 增加AI单元测试
    Change-Id: I8f048bc742d257dccf4c2aa88474523e4bebade5

---
+++ tests/src/game/artificialintelligence/ut_chessfromjudge.h
@@ -0,0 +1,34 @@
+/*
+   * Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+   *
+   * Author:     liyucheng <liyucheng@uniontech.com>
+   *
+   * Maintainer:  liyucheng <liyucheng@uniontech.com>
+   *
+   * This program is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * any later version.
+   *
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+#ifndef UT_CHESSFROMJUDGE_H
+#define UT_CHESSFROMJUDGE_H
+
+#include "pub.h"
+#include "chessfromjudge.h"
+
+#include "gtest/gtest.h"
+
+class UT_ChessFromJudge : public testing::Test
+{
+public:
+};
+
+#endif // UT_CHESSFROMJUDGE_H
